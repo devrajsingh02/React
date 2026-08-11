48f377a8657c1802723f5f3d87a7c9f64ff7e546
devrajsingh02 <devrajsolanki@gmail.com>
2026-08-11 15:01:33 +0530

    first commit

---
+++ 01-folder/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function App() {
+  return (
+    <>
+
+    <div id='dad'>
+      <h1>hello</h1>
+    </div>
+    <div id='chacha'>
+      <h2>heyy</h2>
+    </div>
+    </>
+  )
+}
+
+export default App
